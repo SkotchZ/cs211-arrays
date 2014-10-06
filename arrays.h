9134364c4b228b0c4a5f2659318d4e677d6c1abb
Skachkov Igor <sim5731@yandex.ru>
2014-10-06 18:22:06 +0400

    исправлена реализация task_5_copy: 1 параметр теперь указывает на константыне данные, а переприсвоение указателей изменено на переприсвоение переменных одного массива переменным другого. Объявление task_5_copy добавлено в arrays.h. В main.cpp добавлена проверка на правильность работы task_5_copy.

--- arrays.h
+++ arrays.h
@@ -9,4 +9,6 @@ int task_3_zero();
 
 void task_4();
 
+void task_5_copy(int const * arr1, int * arr2, int size);
+
 #endif arr
--- main.cpp
+++ main.cpp
@@ -1,11 +1,19 @@
 #include <iostream>
 #include "arrays.h"
+#include <cassert>
 
 using namespace std;
+
+int const size = 5;
 
 int main(){
 	task_1();
 	task_2();
 	task_3_zero();
 	task_4();
+	int arr1[size] = { 1, 2, 0, -4, 50 };
+	int arr2[size];
+	task_5_copy(arr1, arr2, size);
+	for (int i = 0; i < size; i++)
+		assert(arr1[i] == arr2[i]);
 }
